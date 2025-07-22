12fd903449695b27c32a114c20b9b99d4009fb84
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-07-22 20:56:38 +0300

    add screen size settings

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,19 @@
+import MovieList from "../components/MovieList"
+import MovieSlider from "../components/MovieSlider"
+import Navbar from "../components/Navbar"
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="min-h-screen">
+      <Navbar />
+
+      <div className="max-w-[1440px] mx-auto">
+        <MovieSlider />
+
+        <h2 className="text-xl font-semibold my-6">Doscover Movies</h2>
+        <MovieList />
+      </div>
+    </div>
   )
 }
 
