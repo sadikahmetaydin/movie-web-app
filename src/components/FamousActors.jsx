48244c7e343db9f441c8706f120a238412527e8c
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-08-04 21:47:21 +0300

    create dynamic actors list

--- src/components/FamousActors.jsx
+++ src/components/FamousActors.jsx
@@ -1,33 +1,30 @@
-const actors = [
-  {
-    name: 'Leonardo DiCaprio',
-    image: 'https://m.media-amazon.com/images/M/MV5BMjI0MTg3MzI0M15BMl5BanBnXkFtZTcwMzQyODU2Mw@@._V1_FMjpg_UX1000_.jpg',
-  },
-  {
-    name: 'Scarlett Johansson',
-    image: 'https://m.media-amazon.com/images/M/MV5BMTM3OTUwMDYwNl5BMl5BanBnXkFtZTcwNTUyNzc3Nw@@._V1_FMjpg_UX1000_.jpg',
-  },
-  {
-    name: 'Keanu Reeves',
-    image: 'https://m.media-amazon.com/images/M/MV5BNDEzOTdhNDUtY2EyMy00YTNmLWE5MjItZmRjMmQzYTRlMGRkXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg'
-  },
-  {
-    name: 'Timothée Chalamet',
-    image: 'https://m.media-amazon.com/images/M/MV5BZTIyZWY4ZjktOGJiZi00NGFkLTllMjctZjJjMmNiMjIxOTY2XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg'
-  },
-  {
-    name: 'Chloë Grace Moretz',
-    image: 'https://m.media-amazon.com/images/M/MV5BMTIyMzYyMjItNGE3YS00N2NiLWFkNGEtZDhiZTI2ODk4Zjc0XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg'
-  }
-];
+import { useEffect, useState } from "react"
+import axios from "axios"
 
 const FamousActors = () => {
+
+  const [actors, setActors] = useState([])
+
+  const randomActors = (actors) => {
+    return [...actors].sort(() => Math.random() - 0.5)
+  }
+
+  useEffect(() => {
+    axios.get(`https://api.themoviedb.org/3/person/popular?api_key=${import.meta.env.VITE_TMDB_API_KEY}`)
+      .then((res) => {
+        const actor = randomActors(res.data.results)
+        const limited = actor.slice(0, 10)
+        setActors(limited)
+      })
+      .catch(err => console.log(err))
+  }, [])
+
   return (
     <div className="py-10">
       <div className="flex flex-wrap justify-center gap-11">
-        {actors.map((actor, index) => (
-          <div className="text-center rounded-lg overflow-hidden shadow-lg hover:scale-105 transition-transform duration-300" key={index}>
-            <img src={actor.image} alt={actor.name} className="w-[250px] h-[350px] object-cover" />
+        {actors.map((actor) => (
+          <div className="text-center rounded-lg overflow-hidden shadow-lg hover:scale-105 transition-transform duration-300" key={actor.id}>
+            <img src={`https://image.tmdb.org/t/p/w300${actor.profile_path}`} alt={actor.name} className="w-[250px] h-[350px] object-cover" />
             <p className="py-2 font-semibold text-sm">{actor.name}</p>
           </div>
         ))}
@@ -35,4 +32,5 @@ const FamousActors = () => {
     </div>
   )
 }
+
 export default FamousActors
