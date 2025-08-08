e8b1c3461ecc027572a647f689daff857b54b419
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-08-08 21:54:15 +0300

    create route path for Discover.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import MovieDetails from "./pages/MovieDetails"
+import Discover from "./components/Discover"
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={ <Home /> } />
         <Route path="/movie/:id" element={ <MovieDetails /> } />
+        <Route path="/discover" element={ <Discover />} />
       </Routes>
     </Router>
   )
