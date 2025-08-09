f88ef3d823e71729e4723dc653eb2d053063d4fd
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-08-09 23:02:32 +0300

    add new route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import MovieDetails from "./pages/MovieDetails"
 import Discover from "./components/Discover"
+import Favorites from "./components/Favorites"
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Route path="/" element={ <Home /> } />
         <Route path="/movie/:id" element={ <MovieDetails /> } />
         <Route path="/discover" element={ <Discover />} />
+        <Route path="/favorites" element={ <Favorites />} />
       </Routes>
     </Router>
   )
